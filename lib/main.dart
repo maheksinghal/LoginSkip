0f40557e73ee2137c759085ec7264953b44d863a
maheksinghal <mahek.ash.singhal@gmail.com>
2025-01-21 19:03:03 +0530

    google analytics added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'my_website.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: 'AIzaSyBYhKnMicD8d2pDC-w_w9Hje0P8oF6A7I4',
+        appId: '1:315628564065:android:ed8388489b31c1ced20420',
+        messagingSenderId: '315628564065',
+        projectId: 'loginskip-bf6ab',
+        storageBucket: 'loginskip-bf6ab.firebasestorage.app',
+      ));
   runApp(const MyApp());
+  //FirebaseAnalytics.instance.logAppOpen();
 }
 
 class MyApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_core
 import flutter_inappwebview_macos
 import geolocator_apple
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
